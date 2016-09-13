582ade6aed5373eabfb66c720c6c2f77fe2a955c
hemantshelar <hemant.shelar@gmail.com>
2016-09-13 15:51:22 +1000

    STS is confogured for MvcApp

--- MyCorpIdentityServer/Config/Clients.cs
+++ MyCorpIdentityServer/Config/Clients.cs
@@ -1,4 +1,5 @@
 ﻿using IdentityServer3.Core.Models;
+using MyCorp.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,17 @@ namespace MyCorpIdentityServer.Config
         public static List<Client> Get()
         {
             List<Client> clients = new List<Client>();
+            Client clientMvc = new Client
+            {
+                ClientId = Constants.MyCorpMvcAppId,
+                ClientName = "My MVC client",
+                Flow = Flows.ClientCredentials,
+                ClientSecrets = new List<Secret>
+                {
+                    new Secret(Constants.MyCorpMvcAppSecret)
+                },
+                AllowedScopes = new List<string> { "testscope" }
+            };
 
             return clients;
         }
