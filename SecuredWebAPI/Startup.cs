b46fb1f4ae3b045b9f4c9e30da2e4cab3e081009
hemantshelar <hemant.shelar@gmail.com>
2016-09-13 15:32:11 +1000

    Secre API complete.

--- SecuredWebAPI/Startup.cs
+++ SecuredWebAPI/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Owin;
 using Owin;
+using IdentityServer3.AccessTokenValidation;
+using MyCorp.Framework;
 
 [assembly: OwinStartup(typeof(SecuredWebAPI.Startup))]
 
@@ -12,6 +14,12 @@ namespace SecuredWebAPI
     {
         public void Configuration(IAppBuilder app)
         {
+
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
+            {
+                Authority = Constants.MyCorpSTS,
+                RequiredScopes = new List<string>() { "testscope" }
+            });
             ConfigureAuth(app);
         }
     }
